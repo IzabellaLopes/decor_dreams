06c4265ba20e0e097a7e81944d89d5089672c813
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2024-02-01 16:23:49 +0000

    Set verbose name for Meta class in Category model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 class Category(models.Model):
     """Model for Category"""
+    
+    class Meta:
+        """Set verbose name"""
+        verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
