49c497fd05c10ff9db5940577e1bbb34c211b19e
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2024-02-13 16:07:39 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OjNBYDpKS7Uim0j409PhFMgq8ZeVdjan0KJTfrPoBgdEs64tNtGZ5A7CCGnjDk9ci6kiGwgQTmIr9PORhnvb7Sw00mH2UZIDD',
+        'client_secret': 'test client secret',
     }
     
     return render(request, template, context)
