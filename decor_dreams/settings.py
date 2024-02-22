36c85c867557540833cf166a802196774c2ab044
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2024-02-22 22:53:35 +0000

    Added confirmation emails

--- decor_dreams/settings.py
+++ decor_dreams/settings.py
@@ -179,6 +179,11 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'freshnest@example.com'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
