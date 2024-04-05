8fd606ab8b5e00bfb4004c817befca69ea37d784
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2024-04-05 11:23:37 +0100

    Added top products

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+"""Home Views"""
 
-# Create your views here.
+from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """ A view to return the index page """
     
-    return render(request, 'home/index.html')
+    top_products = Product.objects.filter(is_top_product=True)[:6]
+    
+    context = {
+        'top_products': top_products,
+    }
+    
+    return render(request, 'home/index.html', context)
--- products/models.py
+++ products/models.py
@@ -34,6 +34,7 @@ class Product(models.Model):
         max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
+    is_top_product = models.BooleanField(default=False, verbose_name="Top Product")
 
     def __str__(self):
         return self.name
